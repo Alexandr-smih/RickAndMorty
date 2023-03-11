d125eb43c093a0e038f5f9927bfc47f576325899
Alexandr-smih <teplac@mail.ru>
2023-03-11 13:51:51 +0300

    Pagination

--- RickAndMorty/APIClient/RMServise.swift
+++ RickAndMorty/APIClient/RMServise.swift
@@ -23,6 +23,7 @@ final class RMServise {
     /// Send Rick and Morty API Call
     ///  - Parameters:
     ///    - request: Request instance
+    ///    - type: The type of object we expect to get back
     ///    - completion: Callback with data or error
     public func execute<T: Codable>(
         _ request: RMRequest,
@@ -34,6 +35,7 @@ final class RMServise {
             return
         }
         
+        print("API Call: \(request.url?.absoluteString ?? "")")
         let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
             guard let data = data, error == nil else {
                 completion(.failure(error ?? RMServiceError.failedToGetData))
